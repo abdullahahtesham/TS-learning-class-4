44e84de98d60bc370d126307cf71582a2413acd3
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-26 12:49:19 +0500

    add these files

---
+++ main.ts
@@ -0,0 +1,17 @@
+let num1 =10
+let num2 = 5
+console.log(num1==num2)
+console.log(num1== 10 && num2==5) //ture both are correct because && is used for the "AND"
+console.log(num1==5 && num2==10)  //ture both are wrong because && is used for the "AND"
+console.log(num1==10 || num2==num1)   //first  is true second is false so this is ture because || is used for the "OR"
+console.log(!(num1==num2))  //"!" is used for NOT operator
+if(num1==10){console.log("Hello I am Abdullah")}
+if(num1==5){console.log("Hello I am Abdullah")} else{console.log("under else")}   // conditions are in the around brackets and the curely bracket is use to print of the code
+if(num1==6){console.log("number is 6")}else{console.log("number is not 6")}
+if(num1==6){console.log("number is 6");}else if(num1==8){console.log("number is not 6")} else if (num1==10){console.log("number is 10")}
+
+
+let  isRaining = true
+if (isRaining) {console.log("Wear a raincoat.");}else console.log("Wear sunglasses");
+let name1: string="Ubaid"
+if(name1=="Ubaid"){console.log("Welcome Ubaid")}else{console.log("not registor")}
